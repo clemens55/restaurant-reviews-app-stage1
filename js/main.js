0449b350588fe3bc49a1e8bfd27c2669ca1961c0
CLEMENS KRAMETTER <clemens.krametter@student.tugraz.at>
2018-10-24 20:35:48 +0200

    Added a11y for better accessibility

--- js/main.js
+++ js/main.js
@@ -181,6 +181,7 @@ createRestaurantHTML = (restaurant) => {
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
+    name.setAttribute('tabindex', '0');
   li.append(name);
 
   const neighborhood = document.createElement('p');
@@ -194,6 +195,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+    more.setAttribute('aria-label', `More details about ${restaurant.name}`)
   li.append(more)
 
   return li
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -114,6 +114,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+    image.alt = `${restaurant.name} Restaurant`;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
@@ -153,6 +154,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h2');
   title.innerHTML = 'Reviews';
+    title.setAttribute('tabindex', '0');
   container.appendChild(title);
 
   if (!reviews) {
@@ -189,6 +191,7 @@ createReviewHTML = (review) => {
   li.appendChild(rating);
 
   const comments = document.createElement('p');
+    comments.classList.add("review-comments")
   comments.innerHTML = review.comments;
   li.appendChild(comments);
 
